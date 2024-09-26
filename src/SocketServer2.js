2248b08128583a89864c48f8c6896e7bc70028ea
rodrigofelipe3 <rodrigo.soeu@hotmail.com>
2024-09-26 15:40:36 -0300

    feat routes

--- src/SocketServer2.js
+++ src/SocketServer2.js
@@ -7,17 +7,17 @@ let receivedChunks = [];
 let totalChunks = 0;
 
 server.on('message', (msg, rinfo) => {
-  const chunkIndex = msg[0]; // Índice do fragmento
-  totalChunks = msg[1];      // Total de fragmentos
-  const chunk = msg.slice(2); // O conteúdo do fragmento
+  const chunkIndex = msg[0]; 
+  totalChunks = msg[1];      
+  const chunk = msg.slice(2); 
 
-  receivedChunks[chunkIndex] = chunk; // Armazena o fragmento no índice correto
+  receivedChunks[chunkIndex] = chunk; 
 
-  // Se todos os fragmentos foram recebidos, junta e salva a imagem
+
   if (receivedChunks.length === totalChunks && !receivedChunks.includes(undefined)) {
     const fullImage = Buffer.concat(receivedChunks);
-    fs.writeFileSync(`frame-${Date.now()}.jpg`, fullImage);  // Salva o frame
-    receivedChunks = [];  // Limpa os fragmentos para a próxima imagem
+    fs.writeFileSync(`frame-${Date.now()}.jpg`, fullImage); 
+    receivedChunks = [];  
   }
 });
 
